2c6ebcb1a924a0f15ffb11ff194f9b6fa13a8042
Jens J Bernardino <44038611+Jerbernard@users.noreply.github.com>
2018-10-11 14:55:57 -0700

    main.dart file
    Skeleton version of app
    - has the home page
    - couple of buttons at the bottom

---
+++ main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlashIt',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('FlashIt'),
+        ),
+        bottomNavigationBar: new BottomAppBar(
+          color: Colors.blue,
+          child: new Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(icon: Icon(Icons.menu), onPressed: () {},),
+              IconButton(icon: Icon(Icons.search), onPressed: () {},),
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
