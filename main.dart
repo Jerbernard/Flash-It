3dc808709bf7d7a12d3fdd9817e21376e49d5f3c
mrdistractions <31258395+mrdistractions@users.noreply.github.com>
2018-10-14 16:15:46 -0700

    slide add page update
    Added a bar bellow slide. and changed the icons.

--- main.dart
+++ main.dart
@@ -25,7 +25,7 @@ class FirstScreen extends StatelessWidget {
                   onPressed: () {
                        Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => SecondScreen()),
+              MaterialPageRoute(builder: (context) => MyCustomForm()),
             );
                   },
                 ),
@@ -62,6 +62,81 @@ class SecondScreen extends StatelessWidget {
           child: Text('Go back!'),
         ),
       ),
+    );
+  }
+}
+
+
+
+
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+// Define a corresponding State class. This class will hold the data related to
+// our Form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller. We will use it to retrieve the current value
+  // of the TextField!
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is disposed
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flash Card Side A:'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: myController,
+        ),
+        
+      ),
+ // BOTTOM NAVIGATION BAR MANAGEMENENT
+      bottomNavigationBar: new BottomAppBar(
+            color: Colors.blue,
+            child: new Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.question_answer),
+                  onPressed: () {   return showDialog(
+            context: context,
+            
+            builder: (context) {
+              return AlertDialog(
+                // Retrieve the text the user has typed in using our
+                // TextEditingController
+                content: Text(myController.text),
+              );
+            },
+          ); },
+                ),
+                IconButton(
+                  icon: Icon(Icons.save),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {Navigator.pop(context);},
+                ),
+                IconButton(
+                  icon: Icon(Icons.home),
+                  onPressed: () { Navigator.pop(context);},
+                ),
+              ],
+            )),
     );
   }
 }
