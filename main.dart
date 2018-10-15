abde6dd71a2678f89f84e7e07d24bbe4855b1723
Tommy Chao <tchao770@gmail.com>
2018-10-15 04:10:43 -0700

    Updated create flashcard page
    included answer field

--- main.dart
+++ main.dart
@@ -31,7 +31,7 @@ class FirstScreen extends StatelessWidget {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => MyApp(storage: TextStorage())),
+                        builder: (context) => AddCard(storage: TextStorage())),
                   );
                 },
               ),
@@ -43,16 +43,16 @@ class FirstScreen extends StatelessWidget {
               IconButton(
                 icon: Icon(Icons.school),
                 tooltip: 'Test Flashcards',
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.menu),
                 onPressed: () {Navigator.push(
                     context,
                     MaterialPageRoute(
                         builder: (context) => ViewCards(storage: TextStorage())),
                   );
                 },
-              ),
-              IconButton(
-                icon: Icon(Icons.menu),
-                onPressed: () {},
               ),
             ],
           )),
@@ -93,25 +93,30 @@ class TextStorage {
   }
 }
 
-class MyApp extends StatefulWidget {
+class AddCard extends StatefulWidget {
   final TextStorage storage;
 
-  MyApp({Key key, @required this.storage}) : super(key: key);
+  AddCard({Key key, @required this.storage}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
+  _AddCardState createState() => _AddCardState();
   
 }
 
 class ViewCards extends StatefulWidget {
   final TextStorage storage;
   ViewCards({Key key, @required this.storage}) : super(key: key);
+  
   @override
   _ViewCards createState() => _ViewCards();
 }
 
-class _MyAppState extends State<MyApp> {
-  TextEditingController _textField = new TextEditingController();
+
+
+
+class _AddCardState extends State<AddCard> {
+  TextEditingController _questionField = new TextEditingController();
+  TextEditingController _answerField = new TextEditingController();
 
   String _content = '';
 
@@ -133,7 +138,14 @@ class _MyAppState extends State<MyApp> {
     return widget.storage.writeFile(text);
   }
 
-  
+  Future<File> _clearContentsInTextFile() async {
+    setState(() {
+      _content = '';
+    });
+
+    return widget.storage.cleanFile();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -145,8 +157,26 @@ class _MyAppState extends State<MyApp> {
         padding: EdgeInsets.all(20.0),
         child: Column(
           children: <Widget>[
-              TextField(	
-              controller: _textField,	
+            TextField(
+              controller: _questionField,
+            ),
+            Text('Question'),
+            TextField(
+              controller: _answerField,
+            ),
+            Text('Answer'),
+            Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: RaisedButton(
+                child: Text(
+                  'Clear Contents',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.redAccent,
+                onPressed: () {
+                  _clearContentsInTextFile();
+                },
+              ),
             ),
             Expanded(
               flex: 1,
@@ -154,20 +184,14 @@ class _MyAppState extends State<MyApp> {
                 child: Text(
                   '$_content',
                   style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.0,
-                    
+                    color: Colors.red,
+                    fontSize: 22.0,
                   ),
-                
                 ),
-                
               ),
-                
             ),
           ],
-          
         ),
-        
       ),
       bottomNavigationBar: new BottomAppBar(
           color: Colors.blue,
@@ -196,10 +220,16 @@ class _MyAppState extends State<MyApp> {
                             new FlatButton(
                                 child: new Text("Yes"),
                                 onPressed: () {
-                                  if (_textField.text.isNotEmpty) {
-                                    _writeStringToTextFile(_textField.text);
-                                    _textField.clear();
+                                  if (_questionField.text.isNotEmpty) {
+                                    _writeStringToTextFile(_questionField.text);
+                                    _questionField.clear();
+                                    _writeStringToTextFile('\n');
                                   }
+                                  if (_answerField.text.isNotEmpty) {
+                                    _writeStringToTextFile(_answerField.text);
+                                    _answerField.clear();
+                                  }
+                                  //_writeStringToTextFile('\n');
                                   Navigator.pop(context);
                                 }),
                             new FlatButton(
@@ -266,26 +296,18 @@ class _ViewCards extends  State<ViewCards>{
   widget.storage.readFile().then((String text){
     setState((){
       _question = text;});
-      });
+  });
   widget.storage.readFile().then((String text){
     setState((){
     _answer = text; });
   });
   }
 
-Future<File> _clearContentsInTextFile() async {
-    setState(() {
-      _question = '';
-    });
-
-    return widget.storage.cleanFile();
-  }
-
 @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('Examine FlashCards'),
+        title: Text('View Flashcards'),
         backgroundColor: Colors.blue,
       ),
       body: Container(
@@ -298,7 +320,7 @@ Future<File> _clearContentsInTextFile() async {
                 child: Text(
                   '$_question',
                   style: TextStyle(
-                    color: Colors.black,
+                    color: Colors.blue,
                     fontSize: 22.0,
                   ),
                 ),
@@ -328,35 +350,11 @@ Future<File> _clearContentsInTextFile() async {
 
               IconButton(
                 icon: Icon(
-                    Icons.delete_forever), //delete current card in progress
+                    Icons.delete_outline), //delete current card in progress
                 tooltip: 'Delete current Flashcard',
-                onPressed: () {
-                  return showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                          content: new Text(
-                              "Are you sure you would like to delete all flashcards?"),
-                          actions: <Widget>[
-                            new FlatButton(
-                                child: new Text("Yes"),
-                                onPressed: () {
-                                  //save here
-                                 _clearContentsInTextFile();
-                                  Navigator.pop(context);
-
-                                }),
-                            new FlatButton(
-                                child: new Text("No"),
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                })
-                          ]);
-                    },
-                  );
-                    },
+                onPressed: () {},
               ),
-            
+
               IconButton(
                 icon: Icon(Icons.home), //return home
                 tooltip: 'Home',
