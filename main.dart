95c9eed9393309493d668ea7d70ecb28bef993a8
mrdistractions <31258395+mrdistractions@users.noreply.github.com>
2018-10-15 01:16:54 -0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -3,65 +3,54 @@ import 'package:path_provider/path_provider.dart';
 import 'dart:async';
 import 'dart:io';
 
-
 void main() {
-   runApp(
-      MaterialApp(
-      title: 'FlashIt',
-      home: FirstScreen(),
-    )
-   );
+  runApp(MaterialApp(
+    title: 'FlashIt',
+    home: FirstScreen(),
+  ));
 }
 
 class FirstScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text('FlashIt'),
-        ),
-
-        bottomNavigationBar: new BottomAppBar(
-            
-            color: Colors.blue,
-            child: new Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.create),
-                  tooltip: 'Create a Flashcard',
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                       MaterialPageRoute(builder: (context) => MyApp(storage: TextStorage())
-                       ),
-                      );
-                  },
-                ),
-
-                IconButton(
-                  icon: Icon(Icons.folder_open),
-                  tooltip: 'Manage Flashcards',
-                  onPressed: () {},
-                ),
-
-                IconButton(
-                  icon: Icon(Icons.school),
-                  tooltip: 'Test Flashcards',
-                  onPressed: () {},
-                ),
-
-                IconButton(
-                  icon: Icon(Icons.menu),
-                  onPressed: () {},
-                ),
-
-              ],
-            )
-          ),
-      );
+      ),
+      bottomNavigationBar: new BottomAppBar(
+          color: Colors.blue,
+          child: new Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.create),
+                tooltip: 'Create a Flashcard',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MyApp(storage: TextStorage())),
+                  );
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.folder_open),
+                tooltip: 'Manage Flashcards',
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.school),
+                tooltip: 'Test Flashcards',
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {},
+              ),
+            ],
+          )),
+    );
   }
 }
 
@@ -70,48 +59,48 @@ class TextStorage {
     final directory = await getApplicationDocumentsDirectory();
     return directory.path;
   }
- 
+
   Future<File> get _localFile async {
     final path = await _localPath;
     return File('$path/text.txt');
   }
- 
+
   Future<String> readFile() async {
     try {
       final file = await _localFile;
- 
+
       String content = await file.readAsString();
       return content;
     } catch (e) {
       return '';
     }
   }
- 
+
   Future<File> writeFile(String text) async {
     final file = await _localFile;
     return file.writeAsString('$text\r\n', mode: FileMode.append);
   }
- 
+
   Future<File> cleanFile() async {
     final file = await _localFile;
     return file.writeAsString('');
   }
 }
- 
+
 class MyApp extends StatefulWidget {
   final TextStorage storage;
- 
+
   MyApp({Key key, @required this.storage}) : super(key: key);
- 
+
   @override
   _MyAppState createState() => _MyAppState();
 }
- 
+
 class _MyAppState extends State<MyApp> {
   TextEditingController _textField = new TextEditingController();
- 
+
   String _content = '';
- 
+
   @override
   void initState() {
     super.initState();
@@ -121,23 +110,23 @@ class _MyAppState extends State<MyApp> {
       });
     });
   }
- 
+
   Future<File> _writeStringToTextFile(String text) async {
     setState(() {
       _content += text + '\r\n';
     });
- 
+
     return widget.storage.writeFile(text);
   }
- 
+
   Future<File> _clearContentsInTextFile() async {
     setState(() {
       _content = '';
     });
- 
+
     return widget.storage.cleanFile();
   }
- 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -153,7 +142,7 @@ class _MyAppState extends State<MyApp> {
               controller: _textField,
             ),
 
-            Padding(
+            /* Padding(
               padding: EdgeInsets.only(bottom: 20.0),
               child: RaisedButton(
                 child: Text(
@@ -165,7 +154,7 @@ class _MyAppState extends State<MyApp> {
                   _clearContentsInTextFile();
                 },
               ),
-            ),
+            ),*/
             Expanded(
               flex: 1,
               child: new SingleChildScrollView(
@@ -181,107 +170,88 @@ class _MyAppState extends State<MyApp> {
           ],
         ),
       ),
-bottomNavigationBar: new BottomAppBar(
-            color: Colors.blue,
+      bottomNavigationBar: new BottomAppBar(
+          color: Colors.blue,
+          child: new Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              //bottom app functionality here
+              IconButton(
+                icon: Icon(Icons.question_answer),
+                tooltip: 'Flip Flashcard',
+                onPressed: () {},
+              ),
 
-            child: new Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-              children: <Widget>[       //bottom app functionality here
-                IconButton(
-                  icon: Icon(Icons.question_answer),
-                  tooltip: 'Flip Flashcard',
-
-            onPressed: () {                },
-                ),
-
-                IconButton(
-                  icon: Icon(Icons.save),   //save the current card
-                  tooltip: 'Save Flashcard',
-                  onPressed: () {
-                    return showDialog(
-                      context: context,
-                      
-                      builder: (context) {
-                        return AlertDialog(
-
-                          content: new Text("Would you like to save this flashcard?"),
+              IconButton(
+                icon: Icon(Icons.save), //save the current card
+                tooltip: 'Save Flashcard',
+                onPressed: () {
+                  return showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                          content: new Text(
+                              "Would you like to save this flashcard?"),
                           actions: <Widget>[
                             new FlatButton(
-                              child: new Text("Yes"),
-                              onPressed: () {
-                                
-                  if (_textField.text.isNotEmpty) {
-                    _writeStringToTextFile(_textField.text);
-                    _textField.clear();
-                  }
-                              Navigator.pop(context);
-                              }
-                            ),
-
+                                child: new Text("Yes"),
+                                onPressed: () {
+                                  if (_textField.text.isNotEmpty) {
+                                    _writeStringToTextFile(_textField.text);
+                                    _textField.clear();
+                                  }
+                                  Navigator.pop(context);
+                                }),
                             new FlatButton(
-                              child: new Text("No"),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              }
-                            )
-                            
-                          ]
-                        );
-                      },
-                    ); 
-                  },
-                ),
+                                child: new Text("No"),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                })
+                          ]);
+                    },
+                  );
+                },
+              ),
 
-                IconButton(
-                  icon: Icon(Icons.delete_outline), //delete current card in progress
-                  tooltip: 'Delete current Flashcard',
-                  onPressed: () {
-                    return showDialog(
-                      context: context,
-                      
-                      builder: (context) {
-                        return AlertDialog(
-                          
-                          content: new Text("Are you sure you would like to delete this flashcard?"),
+              IconButton(
+                icon: Icon(
+                    Icons.delete_outline), //delete current card in progress
+                tooltip: 'Delete current Flashcard',
+                onPressed: () {
+                  return showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                          content: new Text(
+                              "Are you sure you would like to delete this flashcard?"),
                           actions: <Widget>[
                             new FlatButton(
-                              child: new Text("Yes"),
-                              onPressed: () {
-                                //save here
-                                Navigator.pop(context);
-                              }
-                            ),
-
+                                child: new Text("Yes"),
+                                onPressed: () {
+                                  //save here
+                                  Navigator.pop(context);
+                                }),
                             new FlatButton(
-                              child: new Text("No"),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              }
-                            )
-                            
-                          ]
-                        );
-                      },
-                    );                    
-                  },
-                ),
+                                child: new Text("No"),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                })
+                          ]);
+                    },
+                  );
+                },
+              ),
 
-                IconButton(
-                  icon: Icon(Icons.home), //return home
-                  tooltip: 'Home',
-                  onPressed: () { 
-                    Navigator.pop(context);
-                  },
-                ),
-
-
-
-              ],
-             )
-        ),
-
+              IconButton(
+                icon: Icon(Icons.home), //return home
+                tooltip: 'Home',
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          )),
     );
   }
 }
