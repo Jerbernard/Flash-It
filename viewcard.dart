9481ae9f9367c2ff7a96979ce1f5253a1033c41d
mrdistractions <31258395+mrdistractions@users.noreply.github.com>
2018-10-17 02:32:45 -0700

    View Cards

--- viewcard.dart
+++ viewcard.dart
@@ -20,21 +20,16 @@ class _ViewCards extends State<ViewCards> {
   @override
   void initState() {
     super.initState();
-    // widget.storage.readFile().then((String text){
-    //   setState((){
-    //     _question = text});
-    // });
     widget.storage.readFile().then((String text) {
       setState(() {
-        _answer = text;
+        _answer = text; // pulls text from file
       });
-      _question = _answer.split('\n');
+      _question = _answer.split('\n'); // split string to array
     });
   }
 
   Future<File> _clearContentsInTextFile() async {
     setState(() {
-      //  _question = '';
       _answer = '';
     });
 
@@ -51,7 +46,7 @@ class _ViewCards extends State<ViewCards> {
         ],
         backgroundColor: Colors.blue,
       ),
-      body: _buildFlashCardSuggestions(),
+      body: _buildFlashCard(),
       bottomNavigationBar: new BottomAppBar(
         color: Colors.blue,
         child: new Row(
@@ -114,34 +109,94 @@ class _ViewCards extends State<ViewCards> {
     );
   }
 
-  Widget _buildFlashCardSuggestions() {
+  Widget _buildFlashCard() {
     return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (BuildContext _context, int i) {
-          if( i.isOdd){
-            return Divider();
-          } else {
-            return _buildRow(_question[i]);
-          }
-        },
-        itemCount: _question.length,
-        );
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider();
+        } else if (i + 1 != _question.length) {
+          return _buildRow(_question[i], _question[i + 1]);
+        }
+      },
+      itemCount: _question.length,
+    );
   }
 
-  Widget _buildRow(String question){
+  Widget _buildAnswer(String answer, String question) {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(builder: (BuildContext context) {
+        return new Scaffold(
+          appBar: new AppBar(
+            title: const Text('Answer: '),
+          ),
+          body: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 85.0, right: 0.0),
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Q: $question",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20.0,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                ), 
+                Padding(
+                  padding: EdgeInsets.only(left: 90.0, right: 90.0),
+                  child: Text("\n\nA: $answer"),
+                ), 
+              ],
+            ),
+          ),
+          bottomNavigationBar: new BottomAppBar(
+            color: Colors.blue,
+            child: new Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                //bottom app functionality here
+                IconButton(
+                  icon: Icon(Icons.check), //save the current card
+                  tooltip: 'Save Flashcard',
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.home), //return home
+                  tooltip: 'Home',
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      }),
+    );
+  }
+
+  Widget _buildRow(String question, String answer) {
     final bool alreadySaved = _saved.contains(question);
 
     return new ListTile(
       title: new Text(
-        question,
+        '# $question',
         style: _biggerFont,
       ),
       trailing: new Icon(
         alreadySaved ? Icons.check_box : Icons.check_box_outline_blank,
         color: alreadySaved ? Colors.blue : null,
-
       ),
       onTap: () {
+        _buildAnswer(answer, question);
         setState(() {
           if (alreadySaved) {
             _saved.remove(question);
@@ -153,30 +208,29 @@ class _ViewCards extends State<ViewCards> {
     );
   }
 
-
-void _pushSaved() {
+  void _pushSaved() {
     Navigator.of(context).push(
+      // new page
       new MaterialPageRoute<void>(
+        // create material
         builder: (BuildContext context) {
           final Iterable<ListTile> tiles = _saved.map(
             (String question) {
               return new ListTile(
                 title: new Text(
-                  question.toString(),
+                  question.toString(), // print question
                   style: _biggerFont,
                 ),
               );
             },
           );
-          final List<Widget> divided = ListTile
-              .divideTiles(
-                context: context,
-                tiles: tiles,
-              )
-              .toList();
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
           return new Scaffold(
             appBar: new AppBar(
-              title: const Text('Saved Flash Card'),
+              title: const Text('Saved Suggestions'),
             ),
             body: new ListView(children: divided),
           );
