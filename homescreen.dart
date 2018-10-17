5afce5a9dccdbe37a4b4cf729bb79e293d48db0b
Jens J Bernardino <44038611+Jerbernard@users.noreply.github.com>
2018-10-17 01:48:00 -0700

    New homescreen

--- homescreen.dart
+++ homescreen.dart
@@ -2,54 +2,106 @@ import 'package:flutter/material.dart';
 import 'textstorage.dart';
 import 'addcard.dart';
 import 'viewcard.dart';
+import 'dart:io';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('FlashIt'),
+        title: Text('FlashIt v0.1a'),
       ),
-      bottomNavigationBar: new BottomAppBar(
-          color: Colors.blue,
-          child: new Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.create),
-                tooltip: 'Create a Flashcard',
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => AddCard(storage: TextStorage())),
-                  );
-                },
+      body: Container(
+            
+            padding: EdgeInsets.all(20.0),
+            child: Column(              
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 85.0, right: 0.0),
+                  child: RichText(
+                    text: TextSpan(
+                      text: "FlashIt!",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 60.0,
+                        fontStyle: FontStyle.italic,                    
+                      ),
+
+                    ),
+
+                  ),              
+                ),//Flash it text
+                Padding(
+                  padding: EdgeInsets.only(left: 90.0, right: 90.0),
+                  child: Text("How would you like to flash it?"),              
+                ),//Add card b
+                Padding(
+                  padding: EdgeInsets.only(bottom: 20.0),
+                  child: RaisedButton(
+                    child: Text(
+                      'Create New Flashcard',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.blue,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AddCard(storage: TextStorage())),
+                      );
+                    },
+                  ),              
+                ),//Add card button
+                Padding(
+                  padding: EdgeInsets.only(bottom: 20.0),
+                  child: RaisedButton(
+                    child: Text(
+                      'Manage Flashcards',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.blue,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ViewCards(storage: TextStorage())),
+                        );
+                      },
+                    ),              
+                  ),//view button
+                  Padding(
+                  padding: EdgeInsets.only(bottom: 20.0),
+                  child: RaisedButton(
+                    child: Text(
+                      'Test Flashcards',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.blue,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ViewCards(storage: TextStorage())),
+                        );
+                      },
+                    ),              
+                  ),//edit button
+                  Padding(
+                  padding: EdgeInsets.only(bottom: 20.0),
+                  child: RaisedButton(
+                    child: Text(
+                      'Exit',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.blue,
+                    onPressed: () => exit(0), 
+                    ),              
+                  ),//exit button
+                ],
               ),
-              IconButton(
-                icon: Icon(Icons.folder_open),
-                tooltip: 'Manage Flashcards',
-                onPressed: () {Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ViewCards(storage: TextStorage())),
-                  );
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.school),
-                tooltip: 'Test Flashcards',
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.settings),
-                tooltip: 'Settings',
-                onPressed: () {},
-              ),
-            ],
-          )
-        ),
+            ),
     );
   }
 }
