011b4d50e28e335cb6295f323133afccd0f9d4ed
jimmyxuan <jimmyxuan@csu.fullerton.edu>
2018-11-16 17:41:26 -0800

    Update viewcard.dart

--- viewcard.dart
+++ viewcard.dart
@@ -1,55 +1,289 @@
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter/material.dart';
+import 'textstorage.dart';
 import 'dart:async';
 import 'dart:io';
+import 'addcard.dart';
 
-class TextStorage {
-  Future<String> get _qlocalPath async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
+class ViewCards extends StatefulWidget {
+  final TextStorage storage;
+  ViewCards({Key key, @required this.storage}) : super(key: key);
+
+  @override
+  _ViewCards createState() => _ViewCards();
+}
+
+class _ViewCards extends State<ViewCards> {
+  List<String> _card;
+  List<String> _deck;
+  String _deckName;  
+  String _file;
+  final Set<String> _saved = new Set<String>();
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  void initState() {
+    super.initState();
+    widget.storage.readFile().then((String text) {
+      setState(() {
+        _deckName = text;                       // pulls text from file
+      });
+      _deck = _deckName.split('\n');            // split string to array
+    });
   }
 
-  Future<File> get _qlocalFile async {
-    final path = await _qlocalPath; 
-    return File('$path/text.txt');
+  Future<File> _clearContentsInTextFile() async {
+    setState(() {
+      _file = '';
+    });
+
+    return widget.storage.cleanFile();
   }
 
-  Future<String> readFile() async {
-    try {
-      final file = await _qlocalFile;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('View Flashcards'),
+        actions: <Widget>[
+          //new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ],
+        backgroundColor: Colors.blue,
+      ),
+      body: _buildDecks(),
+      bottomNavigationBar: new BottomAppBar(
+        color: Colors.blue,
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            //bottom app functionality here
+            IconButton(
+              icon: Icon(Icons.question_answer),
+              tooltip: 'Flip Flashcard',
+              onPressed: () {},
+            ),
 
-      String content = await file.readAsString();
-      return content;
-    } catch (e) {
-      return '';
-    }
+            IconButton(
+              icon: Icon(Icons.save), //save the current card
+              tooltip: 'Save Flashcard',
+              onPressed: () {},
+            ),
+
+            IconButton(
+              icon:
+                  Icon(Icons.delete_forever), //delete current card in progress
+              tooltip: 'Delete current Flashcard',
+              onPressed: () {
+                return showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                        content: new Text(
+                            "Are you sure you would like to delete all flashcards?"),
+                        actions: <Widget>[
+                          new FlatButton(
+                              child: new Text("Yes"),
+                              onPressed: () {
+                                //save here
+                                _clearContentsInTextFile();
+                                Navigator.pop(context);
+                              }),
+                          new FlatButton(
+                              child: new Text("No"),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              })
+                        ]);
+                  },
+                );
+              },
+            ),
+
+            IconButton(
+              icon: Icon(Icons.plus_one), //return home
+              tooltip: 'Addcard',
+              onPressed: () {
+                
+                    Navigator.pop(context);
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AddCard(storage: TextStorage())),
+                  );
+                  
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
-  Future<File> writeFile(String text) async {
-    final file = await _qlocalFile;
-    return file.writeAsString('$text\r\n', mode: FileMode.append);
+  Widget _buildFlashCard() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider();
+        } else if (i + 1 != _card.length) {
+          return _buildRow(_card[i], _card[i + 1]);
+        }
+      },
+      itemCount: _card.length,
+    );
   }
 
-  Future<File> cleanFile() async {
-    final file = await _qlocalFile;
-    return file.writeAsString('');
+    Widget _buildDecks() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (BuildContext _context, int i) {
+
+          if (i != _deck.length) {
+          Divider(); 
+          return _buildDeckRow(_deck[i]);
+        }
+      },
+      itemCount: _deck.length,
+    );
   }
 
+  Widget _buildAnswer(String answer, String question) {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(builder: (BuildContext context) {
+        return new Scaffold(
+          appBar: new AppBar(
+            title: const Text('Answer: '),
+          ),
+          body: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 85.0, right: 0.0),
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Q: $question",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20.0,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 90.0, right: 90.0),
+                  child: Text("\n\nA: $answer"),
+                ),
+              ],
+            ),
+          ),
+          bottomNavigationBar: new BottomAppBar(
+            color: Colors.blue,
+            child: new Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                //bottom app functionality here
+                IconButton(
+                  icon: Icon(Icons.check), //save the current card
+                  tooltip: 'Mark Flashcard',
+                  onPressed: () {
+                    _saved.add(question);
+                    Navigator.pop(context);
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.backspace), //return home
+                  tooltip: 'back',
+                  onPressed: () {
+                    _saved.remove(question);
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      }),
+    );
+  }
 
-  Future<File> fileLocation (String name) async
+  Widget _buildRow(String question, String answer) {
+    final bool alreadySaved = _saved.contains(question);
+
+    return new ListTile(
+      title: new Text(
+        '$question',
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.check_box : Icons.check_box_outline_blank,
+        color: alreadySaved ? Colors.blue : null,
+      ),
+      onLongPress: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(question);
+          } else {
+            _saved.add(question);
+          }
+        });
+      },
+      onTap: () {
+        _buildAnswer(answer, question);
+      },
+    );
+  }
+
+  Widget _buildDeckRow(String deckName)
   {
-    final path = await _qlocalPath;
-    return File('$path/$name.txt'); 
+    return new ListTile(
+      title: new Text(
+        '$deckName',
+        style: _biggerFont,
+      ),
+      onTap: () {
+      widget.storage.readDeck(deckName).then((String text) {
+        setState(() {
+          _file = text; 
+        });
+        _card = _file.split('\n');
+      });                                   //would have to build flashcards here? make a new scaffold? a new page? team?
+      }
+    );
   }
 
-  Future<String> readDeck(String filename) async {
-    try {
-      final file = await fileLocation(filename);
-
-      String content = await file.readAsString();
-      return content;
-    } catch (e) {
-      return '';
-    }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      // new page
+      new MaterialPageRoute<void>(
+        // create material
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (String question) {
+              return new ListTile(
+                title: new Text(
+                  question.toString(), // print question
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+          return new Scaffold(
+            appBar: new AppBar(
+              title: const Text('Saved Questions'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
+      ),
+    );
   }
-
-} 
+}
