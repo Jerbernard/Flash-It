e759fafd01b60c22ddaf33866a73e9e06e759e70
jimmyxuan <jimmyxuan@csu.fullerton.edu>
2018-11-17 17:36:54 -0800

    Update homescreen.dart

--- homescreen.dart
+++ homescreen.dart
@@ -6,6 +6,7 @@ import 'testcard.dart';
 import 'dart:io';
 
 class HomeScreen extends StatelessWidget {
+  TextEditingController _name = new TextEditingController(); 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +14,6 @@ class HomeScreen extends StatelessWidget {
         title: Text('FlashIt v0.1a'),
       ),
       body: Container(
-            
             padding: EdgeInsets.all(20.0),
             child: Column(              
               mainAxisAlignment: MainAxisAlignment.center,
@@ -28,10 +28,8 @@ class HomeScreen extends StatelessWidget {
                         color: Colors.blue,
                         fontSize: 60.0,
                         fontStyle: FontStyle.italic,                    
-                      ),
-
+                      )
                     ),
-
                   ),              
                 ),//Flash it text
                 Padding(
@@ -42,18 +40,51 @@ class HomeScreen extends StatelessWidget {
                   padding: EdgeInsets.only(bottom: 20.0),
                   child: RaisedButton(
                     child: Text(
-                      'Create New Flashcard',
+                      'Create New Deck',
                       style: TextStyle(color: Colors.white),
                     ),
                     color: Colors.blue,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => AddCard(storage: TextStorage())),
+                    onPressed: ()  {
+                      showDialog<String> (context: context, 
+                      child: new AlertDialog(
+                        contentPadding: const EdgeInsets.all(16),
+                        
+                        content: new Row(children: <Widget> [
+                          new Expanded (
+                            child: new TextField 
+                            (controller: _name,
+                            decoration: new InputDecoration(
+                              labelText: 'Enter Deck Name',)
+                            )
+                            ,)
+                        ],
+                        ),
+                      
+                      actions: <Widget> [
+                        new FlatButton (
+                          child: const Text('CANCEL'),
+                          onPressed:() {
+                            _name.clear();
+                            Navigator.pop(context);
+                          }
+                        ),
+                        new FlatButton(
+                          child:const Text('ENTER'),
+                          onPressed: ()
+                          {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => AddCard(storage: TextStorage())),
+                          );
+                          })
+                      ],
+                      ),
                       );
-                    },
-                  ),              
+                      }
+                      ,
+                  ),   
+                           
                 ),//Add card button
                 Padding(
                   padding: EdgeInsets.only(bottom: 20.0),
