fb21b28a94ef7085b0c8823fd5607130fbf906ae
Jens J Bernardino <44038611+Jerbernard@users.noreply.github.com>
2018-11-18 20:10:21 -0800

    Update textstorage.dart

--- textstorage.dart
+++ textstorage.dart
@@ -9,8 +9,8 @@ class TextStorage {
   }
 
   Future<File> get _qlocalFile async {
-    final path = await _qlocalPath;
-    return File('$path/text.txt');
+    final path = await _qlocalPath; 
+    return File('$path/decks.txt');
   }
 
   Future<String> readFile() async {
@@ -26,11 +26,37 @@ class TextStorage {
 
   Future<File> writeFile(String text) async {
     final file = await _qlocalFile;
-    return file.writeAsString('$text\r\n', mode: FileMode.append);
+    return file.writeAsString('$text\n', mode: FileMode.append);
   }
 
   Future<File> cleanFile() async {
     final file = await _qlocalFile;
     return file.writeAsString('');
   }
-}
+
+
+  Future<File>  fileLocation (String name) async
+  {
+    final path = await _qlocalPath;
+    return File('$path/$name.txt'); 
+  }
+
+  Future<String> readDeck(String filename) async {
+    try {
+      final path = await _qlocalPath;
+      var file = new File ('$path/$filename.txt'); 
+      String content = await file.readAsString();
+      return content;
+    } catch (e) {
+      return '';
+    }
+  }
+
+  Future<File> writeDeck(String text, String deckname) async
+  {
+   final path = await _qlocalPath;
+   var file = new File ('$path/$deckname.txt'); 
+   return file.writeAsString('$text\r\n', mode: FileMode.append); 
+  }
+
+} 
