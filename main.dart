0fa31cfece0a96dddad2e366486ade6f4cde7dde
go-hyun77 <37429855+go-hyun77@users.noreply.github.com>
2018-12-10 16:56:58 -0800

    dark brightness here

--- main.dart
+++ main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'homescreen.dart';
 
+
 void main() {
   runApp(MaterialApp(
+      theme: ThemeData(     //edit brightness here
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        accentColor: Colors.blue
+      ),
       title: 'FlashIt',
       home: HomeScreen(),
   ));
